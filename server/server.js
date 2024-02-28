187c1b280426a8e368aca94b1134257dee1ddf4f
Nicholas Vacca <nicholasfvacca@gmail.com>
2024-02-28 10:45:42 -0500

    server is running

--- server/server.js
+++ server/server.js
@@ -2,15 +2,14 @@ const express = require('express')
 const db = require('./config/connection')
 const path = require('path')
 
-const { AplloServer } = require('@apollo/server')
+const { ApolloServer } = require('@apollo/server')
 const { expressMiddleware } = require('@apollo/server/express4')
 
 const app = express()
 const PORT = process.env.PORT || 3333
 
 
-// Middleware
-app.use(express.json())
+
 
 const typeDefs = `
   type Query {
@@ -27,28 +26,38 @@ const resolvers = {
 }
 
 async function startServer() {
-  const server = new AplloServer({
+  const server = new ApolloServer({
     typeDefs,
     resolvers
   })
-}
 
-app.use('/api', api_routes)
+  await server.start()
+
+  // Middleware
+  app.use(express.json())
+
+  // Apollo/GraphQl MiddleWare
+  app.use('/graphql', expressMiddleware(server))
 
 
 
-if(process.env.NODE_ENV === 'production') {
-  app.use(express.static('../client/dist'))
-  
-  app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, '../client/dist/index.html'))
+  if (process.env.NODE_ENV === 'production') {
+    app.use(express.static('../client/dist'))
+
+    app.get('*', (req, res) => {
+      res.sendFile(path.join(__dirname, '../client/dist/index.html'))
+    })
+  }
+
+
+  db.on('open', () => {
+    app.listen(PORT, () => {
+      console.log(`Server is running on http://localhost:${PORT}`)
+    })
+
   })
 }
 
+startServer()
 
-db.on('open', () => {
-  app.listen(PORT, () => {
-    console.log(`Server is running on http://localhost:${PORT}`)
-  })
-  
-})
+
